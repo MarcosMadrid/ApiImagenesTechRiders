57926ffdcec259d2da0442ff751bed648c398b70
Marcos Madrid Béjar <marcos.madrid@tajamar365.com>
2024-05-14 09:37:08 +0200

    cc

--- ApiImagenesTechRiders/Helpers/HelperFilesManager.cs
+++ ApiImagenesTechRiders/Helpers/HelperFilesManager.cs
@@ -11,7 +11,7 @@ namespace ApiImagenesTechRiders.Helpers
         public HelperFilesManager(string absolutePath, string urlServerHost)
         {
             this.urlServerHost = urlServerHost;
-            List<string> folders = absolutePath.Split("/").ToList();
+            List<string> folders = absolutePath.Split(Path.DirectorySeparatorChar).ToList();
             folders.RemoveRange(folders.Count - 4, 4);
             folders.Add("imgs");
             pathImgs = Path.Combine(folders.ToArray());
